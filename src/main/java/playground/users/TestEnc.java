992b455f00c6cce9d7ff3fa1e1ffd2efc6186a36
Xingjian Xu <xingjian@imnu.edu.cn>
2022-05-16 14:14:16 +0800

    Update

--- src/main/java/playground/users/TestEnc.java
+++ src/main/java/playground/users/TestEnc.java
@@ -1,26 +1,16 @@
 package playground.users;
 
-import java.io.UnsupportedEncodingException;
-import java.math.BigInteger;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import org.mindrot.jbcrypt.BCrypt;
 
 public class TestEnc {
 
 	public static void main(String[] args) {
-		String s = "12_3a4B56789";
+		String password = "12_3a4B56789";
 
-		try {
-			MessageDigest md5 = MessageDigest.getInstance("md5");
-			byte[] digest = md5.digest(s.getBytes("utf-8"));
-			String e = new BigInteger(1, digest).toString(16);
-			System.out.println(e);
-		} catch (NoSuchAlgorithmException e1) {
-			e1.printStackTrace();
-		} catch (UnsupportedEncodingException e1) {
-			e1.printStackTrace();
-		}
-
+		String hashed = BCrypt.hashpw(password, BCrypt.gensalt());
+		
+		System.out.println(BCrypt.checkpw(password, hashed));
+		
 	}
 
 }
